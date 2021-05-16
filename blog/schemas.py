372bc763ab209fe1fe183f7c8b7f6adc9ff0fa74
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-15 20:42:48 -0400

    Added logic and comments to set up table relationships

--- blog/schemas.py
+++ blog/schemas.py
@@ -8,6 +8,9 @@ You use a Pydantic model to create the way the response body is returned,
 	i.e. what the user will see from the database in the response body.
 """
 
+# Import List from typing library
+from typing import List
+
 # Import BaseModel from pydantic for the class
 from pydantic import BaseModel
 
@@ -19,6 +22,39 @@ class Blog(BaseModel):
 	# Define the data types of the fields returned to the user
 	title: str
 	body: str
+	user_id: int
+
+	# Create a new class for configuration
+	class Config():
+		# Need to set the orm_mode to True or it will raise an error
+		orm_mode = True
+
+
+# Create another class for the User schema
+class User(BaseModel):
+	# Define the data types of the fields to return to the user
+	name: str
+	email: str
+	password: str
+
+
+# Create another class to get just the user name
+class UserId(BaseModel):
+	id: int
+
+
+# Create another class for the User schema to return only name and email
+class ShowUser(BaseModel):
+	# Define the data types of the fields to return to the user
+	name: str
+	email: str
+	# Show all of the user's blogs as a list
+	blogs: List[Blog] = []
+
+	# Create a new class for configuration
+	class Config():
+		# Need to set the orm_mode to True or it will raise an error
+		orm_mode = True
 
 
 # Create another schema to show the blog data to the user, and inherit from
@@ -35,26 +71,9 @@ class Blog(BaseModel):
 class ShowBlog(BaseModel):
 	# Define only what you want the user to see
 	title: str
-
-	# Create a new class for configuration
-	class Config():
-		# Need to set the orm_mode to True or it will raise an error
-		orm_mode = True
-
-
-# Create another class for the User schema
-class User(BaseModel):
-	# Define the data types of the fields to return to the user
-	name: str
-	email: str
-	password: str
-
-
-# Create another class for the User schema to return only name and email
-class ShowUser(BaseModel):
-	# Define the data types of the fields to return to the user
-	name: str
-	email: str
+	body: str
+	# Show the user that created the blog
+	creator: ShowUser
 
 	# Create a new class for configuration
 	class Config():
