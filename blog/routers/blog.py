85af5430f9a2cafed1cea027eebf782ccbe87a2e
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-15 22:14:37 -0400

    Removed prefix from routes/paths and added it to the APIRouter parameter.

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -9,7 +9,10 @@ from sqlalchemy.orm import Session
 from .. import schemas, database, models
 
 # Initialize the APIRouter
-router = APIRouter(tags=['Blogs'])
+router = APIRouter(
+	prefix='/blog',
+	tags=['Blogs']
+)
 
 # Create a variable for the get_db function
 get_db = database.get_db
@@ -21,7 +24,7 @@ Then paste them below and change `app` to `router` --v
 
 
 # Add a new route/path request method to get all the blogs from the database
-@router.get('/blog', response_model=List[schemas.ShowBlog])
+@router.get('/', response_model=List[schemas.ShowBlog])
 # Define the method to get all the blogs
 def get_blogs(db: Session = Depends(get_db)):
 	# Query the database to get all the blogs
@@ -36,7 +39,7 @@ def get_blogs(db: Session = Depends(get_db)):
 # If you do not know the correct status code, you can use the status method
 #   from fastapi that has a list of all the status codes and their meanings,
 #   like below.
-@router.post('/blog', status_code=status.HTTP_201_CREATED)
+@router.post('/', status_code=status.HTTP_201_CREATED)
 # Create a function to create a new blog post, taking in the title and body
 #   of the blog using pydantic.
 # Doing it this way will give you a request body
@@ -60,7 +63,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # Create a new path request method to remove a blog
-@router.delete('/blog/{blog_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{blog_id}', status_code=status.HTTP_204_NO_CONTENT)
 # Create the method to remove it from the database
 def destroy(blog_id, db: Session = Depends(get_db)):
 	# Use the query function to locate the blog
@@ -85,7 +88,7 @@ def destroy(blog_id, db: Session = Depends(get_db)):
 
 
 # Create a new path to update a blog based on its id
-@router.put('/blog/{blog_id}', status_code=status.HTTP_202_ACCEPTED)
+@router.put('/{blog_id}', status_code=status.HTTP_202_ACCEPTED)
 # Create the method to update the blog
 # The request: schemas.Blog is just getting the schema of the blog table to
 #   create the proper output format on the api.
@@ -122,7 +125,7 @@ def update(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # Add a new route/path request to get a single blog by its id
-@router.get('/blog/{blog_id}', status_code=200, response_model=schemas.ShowBlog)
+@router.get('/{blog_id}', status_code=200, response_model=schemas.ShowBlog)
 # Define the method to get the 1st blog by the id
 def show_blog(blog_id, db: Session = Depends(get_db)):
 	# Query the database to get the 1st instance of the blog at specified id
