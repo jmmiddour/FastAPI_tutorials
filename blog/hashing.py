bbb698a3ccd492d59d80cd3514738c7f20bfd0d9
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-17 12:05:51 -0400

    Added verify method

--- blog/hashing.py
+++ blog/hashing.py
@@ -16,3 +16,13 @@ class Hash():
 	def bcrypt(password: str):
 		# Hash the password when it is passed in by the user
 		return pwd_cxt.hash(password)
+
+	def verify(hashed_pwd, plain_pwd):
+		"""
+		Function to verify the stored hashed password matches the hash for
+			the inputted password.
+		:param hashed_pwd: str: hashed password that is stored in the database
+		:param plain_pwd: user inputted password through the API
+		:return: bool: True if there is a match, False if there is no match.
+		"""
+		return pwd_cxt.verify(plain_pwd, hashed_pwd)
