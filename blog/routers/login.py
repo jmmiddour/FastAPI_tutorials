76bea148687b400c7e8a3f18657f6ee59c53e5d3
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-17 12:06:26 -0400

    Added logic for login

---
+++ blog/routers/login.py
@@ -0,0 +1,37 @@
+"""
+The file contains
+"""
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from .. import schemas, database, models
+from ..hashing import Hash
+
+router = APIRouter(
+	tags=['Authentication']
+)
+
+
+@router.post('/login')
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+	user = db.query(models.User).filter(
+		models.User.email == request.username).first()
+
+	if not user:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail='Invalid Username (hint: email)! Please try again!'
+		)
+
+	if not Hash.verify(user.password, request.password):
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail='Invalid Password! Please try again!'
+		)
+
+	# Generate a JWT token and return
+
+
+	return user
+
