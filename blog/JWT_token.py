98d26e9e09979956cd6c76e29f97124b788cd750
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-17 18:11:56 -0400

    Removed JWTError from imports, not using.

--- blog/JWT_token.py
+++ blog/JWT_token.py
@@ -3,7 +3,7 @@ This file contains everything for generating the JWT_token
 """
 # Imports
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
