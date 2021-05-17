b48871ac6112e5e2c7505d4d1211b397e70de64f
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-17 18:06:45 -0400

    Added imports and the create access token function

--- blog/JWT_token.py
+++ blog/JWT_token.py
@@ -1,3 +1,32 @@
+"""
+This file contains everything for generating the JWT_token
+"""
+# Imports
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+	"""
+	Function to create a JWT token.
+
+	:param data: dict: the data to be encoded
+	:param expires_delta: Expiration time on the token
+	:return: data encoded as a JWT token
+	"""
+	to_encode = data.copy()
+
+	if expires_delta:
+		expire = datetime.utcnow() + expires_delta
+
+	else:
+		expire = datetime.utcnow() + timedelta(minutes=15)
+
+	to_encode.update({"exp": expire})
+	encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+	return encoded_jwt
